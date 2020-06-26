88c91971e91996b7411da9db926e677b2762f946
israel david <davidisrael194@gmail.com>
2020-06-26 18:36:54 +0100

    setup

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <form @submit.prevent="addTodo">
+      <input type="text" name="title" v-model="title" placeholder="Add todo..." />
+      <input type="submit" value="Add" class="btn" />
+    </form>
+  </div>
+</template>
+
+<script>
+// import { v4 as uuidv4 } from "uuid";
+
+export default {
+  name: "AddTodo",
+  data() {
+    return {
+      title: "",
+      newTodo: {}
+    };
+  },
+  methods: {
+    addTodo() {
+      this.newTodo = {
+        // id: uuidv4(),
+        title: this.title,
+        completed: false,
+      };
+
+      this.$emit("add-todo", this.newTodo);
+      this.title = ''
+    },
+  },
+};
+</script>
+
+<style>
+form {
+  display: flex;
+}
+input[type="text"] {
+  flex: 10;
+  padding: 5px;
+}
+input[type="submit"] {
+  flex: 2;
+}
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-for="todo in todos" :key="todo.id">
+      <!-- {{ todo.title }} -->
+      <TodoItem :todo="todo" @del-todo="$emit('del-todo', todo.id)"/>
+      <!-- <TodoItem :todo="todo" v-on:del-todo="$emit('del-todo', todo.id)"/> -->
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem'
+
+export default {
+  name: "Todos",
+  components: {
+    TodoItem
+  },
+  props: ["todos"]
+};
+</script>
+
+<style scoped></style>
